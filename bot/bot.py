8a06d1600877ef1700ac4f43bbef024fab201835
Geoffrey Daniels <geoff@gpdaniels.com>
2022-11-24 21:15:20 +0000

    Remove old code.

--- bot/bot.py
+++ bot/bot.py
@@ -6,8 +6,6 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-
-#client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 ################################################################################
