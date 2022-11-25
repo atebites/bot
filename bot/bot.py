ae75e97fa81960fd74bbb0f9cbe70f30a1d87a3e
Geoffrey Daniels <geoff@gpdaniels.com>
2022-11-25 09:45:25 +0000

    Replacing ' with " in strings.

--- bot/bot.py
+++ bot/bot.py
@@ -6,38 +6,38 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 ################################################################################
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user}')
+    print(f"Logged in as {bot.user}")
     await bot.change_presence(status=discord.Status.online)
 
 #@bot.event
 #async def on_message(message):
 #    if message.author == bot.user:
 #        return
-#    if message.content.startswith('!hello'):
-#        await message.channel.send('Hello!')
+#    if message.content.startswith("!hello"):
+#        await message.channel.send("Hello!")
 
 ################################################################################
 
 @bot.command()
 async def restart(ctx):
-    if str(ctx.author.id) != os.getenv('ADMIN_ID'):
-        await ctx.send(f'Your ID "{str(ctx.author.id)}" does not have permission to run this command.')
+    if str(ctx.author.id) != os.getenv("ADMIN_ID"):
+        await ctx.send(f"Your ID '{str(ctx.author.id)}' does not have permission to run this command.")
         return
-    await ctx.send(f'Restarting bot...')
+    await ctx.send(f"Restarting bot...")
     await bot.close()
-    os.execv(sys.executable, ['python'] + sys.argv)
+    os.execv(sys.executable, ["python"] + sys.argv)
     os._exit(1)
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send("pong")
 
 ################################################################################
 
-bot.run(os.getenv('TOKEN'))
+bot.run(os.getenv("TOKEN"))
